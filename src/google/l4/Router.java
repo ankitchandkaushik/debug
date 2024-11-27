f4ca7e9f0eff315003581f3009abb8618f886fef
Ankit Chand <ankit.kaushik.33671@gmail.com>
2024-11-27 09:14:01 +0530

    added another questions

--- src/google/l4/Router.java
+++ src/google/l4/Router.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/*
+Preliminary Screening
+
+
+Let's define a kind of message called "Broadcast & Shut Down." When a router receives this message, it broadcasts the same message to all other routers within its wireless range. Then, that router shuts down, and can no longer send or receive messages.
+
+
+For example, Router A is at (0, 0); Router B is at (0, 8); Router C is at (0, 17); Router D is at (11, 0). If the wireless range is 10, when Router A sends a message, it could first reach B; the message from Router B would further reach Router C but Router D would never receive this message.
+Given a list of routers' locations (their names and the corresponding 2D coordinates), a source router, a destination router, and the wireless range, tell me whether a message from the source router can reach the destination router. Write a method / function with appropriate input and output arguments.
+ */
+
+
+
+
 public class Router {
 
   public static class Pair {
